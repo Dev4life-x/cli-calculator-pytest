8031f585dc012349882b8fd954bcc12112770087
cashdev <ai_dinero@outlook.com>
2026-06-26 10:24:49 -0400

    Add if elif for calculator operations

--- calculator.py
+++ calculator.py
@@ -1,10 +1,42 @@
-#CLI calculator project.
-x = int(input("Enter an integer, (x): "))
-y = int(input("Enter an integer, (y): "))
+# CLI calculator project.
 
-operation = "+"
-result = x + y
+import sys
+while True:
 
-print(f"{x} {operation} {y} = {result}")
-print()
+    x_text = input("Enter (x) integer or q to quit: ")
+
+    if x_text == "q":
+        break
+    
+    x = int(x_text)
+
+    y = int(input("Enter an integer, (y): "))
+    operation = input("Enter an operation, (+, -, *, /): ")
+
+    if operation == "+":
+        
+        result = x + y
+        print()
+
+    elif operation == "-":
+        
+        result = x - y
+        print()
+
+    elif operation == "*":
+        
+        result = x * y
+        print()
+    
+    elif operation == "/":
+      
+        result = x / y
+        print()
+    
+    else:
+        print("Invalid operation!")
+        continue
+
+    print(f"{x} {operation} {y} = {result}")
+    print("_ _" * 40 )
 
