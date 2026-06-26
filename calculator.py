1e28e2e837833ad81ab6e4f470b9c1c0ef3d2609
cashdev <ai_dinero@outlook.com>
2026-06-26 07:29:58 -0400

    Hardcode calculator operations

---
+++ calculator.py
@@ -0,0 +1,40 @@
+#CLI calculator project.
+x = 10
+y = 11
+
+operation = "+"
+result = x + y
+
+print(f"{x} {operation} {y} = {result}")
+print()
+
+x = 9.9
+y = 0.9
+
+operation = "-"
+result = x - y
+
+print(f"{x} {operation} {y} = {result}")
+print()
+
+x = 2
+
+y = 99
+
+operation = "*"
+result = x * y
+
+print(f"{x} {operation} {y} = {result}")
+print()
+
+
+x = 77
+
+y = 6
+
+operation = "/"
+result = x / y
+
+print(f"{x} {operation} {y} = {result:.1f}")
+
+
